613de5f9e38fd8f501ee0b6c93dd320dc061d084
Анна <vindanka@gmail.com>
2024-10-04 17:16:24 +0300

    Create 2 threads for print names and ids with intervals

--- Task-8/Program.cs
+++ Task-8/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿var thread1 = new Thread(() =>
+{
+    for(int i = 0; i < 10; i++)
+    {
+        Console.WriteLine(Thread.CurrentThread.Name + "\t" + Thread.CurrentThread.ManagedThreadId);
+        Thread.Sleep(500);
+    }
+});
+
+var thread2 = new Thread(() =>
+{
+    for (int i = 0; i < 10; i++)
+    {
+        Console.WriteLine(Thread.CurrentThread.Name + "\t" + Thread.CurrentThread.ManagedThreadId);
+        Thread.Sleep(500);
+    }
+});
+
+thread1.Name = "thread 1";
+thread2.Name = "thread 2";
+
+thread1.Start();
+thread2.Start();
+
+thread1.Join();
+thread2.Join();
