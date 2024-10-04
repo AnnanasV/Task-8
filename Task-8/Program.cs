4e0d853197f82516d0027337545f55d6ff0be407
Анна <vindanka@gmail.com>
2024-10-04 17:37:24 +0300

    Create 2 threads to increment 1 number, lock it to avoid conflicts

--- Task-8/Program.cs
+++ Task-8/Program.cs
@@ -27,6 +27,7 @@ thread1.Join();
 thread2.Join();
 */
 
+/*
 for(int i = 0; i < 10; i++)
 {
     int taskNum = i;
@@ -39,4 +40,30 @@ Thread.Sleep(1000);
 static void Task(object number)
 {
     Console.WriteLine($"Number: {number} Thread: {Thread.CurrentThread.ManagedThreadId}");
+}
+*/
+
+object lockedObj = new object();
+int number = 0;
+
+Thread thread1 = new Thread(incrementNumber);
+Thread thread2 = new Thread(incrementNumber);
+
+thread1.Start();
+thread2.Start();
+thread1.Join();
+thread2.Join();
+
+Console.WriteLine($"number = {number}");
+
+void incrementNumber()
+{
+    for (int i = 0; i < 1000; i++)
+    {
+        lock (lockedObj)
+        {
+            number++;
+        }
+    }
+    Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} ended its work");
 }
